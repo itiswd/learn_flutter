ed244172535eeefcbf96d60cc2949d48d21d61e5
itiswd <ibrahimtharwat3344@gmail.com>
2025-07-21 11:12:22 +0300

    Implement login functionality using Cubit pattern and remove counter-related code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:learn_flutter/bloc/cubit.dart';
-import 'package:learn_flutter/home.dart';
+
+import 'cubit/login_cubit.dart';
+import 'screens/login_page.dart';
 
 void main() {
-  runApp(BlocProvider(create: (_) => CounterCubit(), child: const MyApp()));
+  runApp(BlocProvider(create: (_) => LoginCubit(), child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Login with Cubit', home: const LoginPage());
+  }
 }
